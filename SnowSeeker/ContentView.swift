423b29001c0ed99a34d88e54e32330330ce3d7e5
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-05 08:01:43 -0400

    Built the list of locations for the app

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink {
+                    Text(resort.name)
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1)
+                        )
+                    
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
         }
-        .padding()
     }
 }
 
---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,17 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Jordan Haynes on 10/5/23.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id, name, country, description, imageCredit: String
+    let price, size, snowDepth, elevation, runs: Int
+    let facilities: [String]
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+}
